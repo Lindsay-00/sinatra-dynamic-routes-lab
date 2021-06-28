2b024a505d450aed09e84df8c1eb4b178a7ba16e
Lindsay <lindsay.chen@yale.edu>
2021-06-29 05:16:40 +0800

    commit tree d6509686df0230b9102ad928c2b700dd01a18181

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     num = params[:number].to_i
-    phrase = params[:phrase]
-    phrase * num 
+    params[:phrase] * num
+
   end
 
 
